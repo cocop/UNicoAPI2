1da56c18cbdaad985a7cd5471b182633ca0ea691
cocop <rmecila@gmail.com>
2016-01-16 22:12:37 +0900

    ver 0.1.0.0

--- Tester/UnitTest.cs
+++ Tester/UnitTest.cs
@@ -12,25 +12,6 @@ namespace Tester
         [TestMethod]
         public async void TestMethod1()
         {
-            var t = new CancellationTokenSource();
-            var nsp = new UNicoAPI2.NicoServicePage();
-            var ls = nsp.Login("nagatoyukiz@yahoo.co.jp", "asdfghjkl");
-            var lr = await ls.RunAsync(ls.UntreatedCount, t.Token);
-
-            var nvsp = nsp.GetVideoServicePage();
-            var ss = nvsp.Search(
-                "ゆっくり実況プレイ",
-                1,
-                SearchType.Tag,
-                new SearchOption()
-                {
-                    SortTarget = SortTarget.PostTime,
-                    SortOrder = SortOrder.Up,
-                });
-
-            var sr = await ss.RunAsync(ss.UntreatedCount, t.Token);
-
-            t.Cancel();
         }
     }
 }
