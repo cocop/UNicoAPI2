9f9c9a473ae47ef1171040c4517a6ba7592f8973
cocop <rmecila@gmail.com>
2023-01-20 01:32:53 +0900

    検索ページが変更されたので対応。パース方法を正規表現からAngleSharpへ変更

--- UNicoAPI2/APIs/html/search_page/Parser.cs
+++ UNicoAPI2/APIs/html/search_page/Parser.cs
@@ -2,12 +2,11 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
+
 namespace UNicoAPI2.APIs.html.search_page
 {
     public class Parser : IHtmlParser<Dictionary<string, string>[]>
     {
-        static readonly Regex videoInfoRegex = new Regex("<p class=\"itemTime\">.*?<span class=\"time\">[ \n]*?(?<time>.*?)[ \n]*?</span>.*?</p>.*?data-id=\"(?<id>.*?)\">.*?<img .*?data-original=\"(?<thumbnail>.*?)\".*?>.*?<span class=\"videoLength\">(?<length>.*?)</span>.*?<p class=\"itemTitle\">.*?<a.*?title=\"(?<title>.*?)\".*?>.*?</a>.*?<p.*?>(?<short_desc>.*?)</p>.*?<li class=\"count view\">.*?<span class=\"value\">(?<view>.*?)</span>.*?</li>.*?<li class=\"count comment\">.*?<span class=\"value\">(?<comment>.*?)</span>.*?</li>.*?<li class=\"count like\">.*?<span class=\"value\">(?<like>.*?)</span>.*?</li>.*?<li class=\"count mylist\">.*?<span class=\"value\">(?<mylist>.*?)</span>.*?</li>", RegexOptions.Singleline);
-
         public string Parse(byte[] Value)
         {
             return Encoding.UTF8.GetString(Value);
@@ -16,23 +15,26 @@ namespace UNicoAPI2.APIs.html.search_page
         public Dictionary<string, string>[] Parse(string Value)
         {
             var result = new List<Dictionary<string, string>>();
-            foreach (Match item in videoInfoRegex.Matches(Value))
+            var htmlParser = new AngleSharp.Html.Parser.HtmlParser();
+            var htmlDocuments = htmlParser.ParseDocument(Value);
+
+            foreach (var item in htmlDocuments.QuerySelectorAll(@".videoList > ul[class=""list""] > .item"))
             {
                 var info = new Dictionary<string, string>();
 
-                info["id"] = item.Groups["id"].Value;
-                info["title"] = item.Groups["title"].Value;
-                info["short_desc"] = item.Groups["short_desc"].Value;
-                info["length"] = item.Groups["length"].Value;
-                info["view"] = item.Groups["view"].Value.Replace(",", "");
-                info["comment"] = item.Groups["comment"].Value.Replace(",", "");
-                info["like"] = item.Groups["like"].Value.Replace(",", "");
-                info["mylist"] = item.Groups["mylist"].Value.Replace(",", "");
+                info["id"] = item.GetAttribute("data-video-id");
+                info["time"] = item.QuerySelector(".time")?.InnerHtml;
+                info["title"] = item.QuerySelector(".itemTitle > a")?.GetAttribute("title");
+                info["short_desc"] = item.QuerySelector(".itemDescription")?.InnerHtml;
+                info["length"] = item.QuerySelector(".videoLength")?.InnerHtml;
+                info["view"] = item.QuerySelector(".count.view > .value")?.InnerHtml?.Replace(",", "");
+                info["comment"] = item.QuerySelector(".count.comment > .value")?.InnerHtml?.Replace(",", "");
+                info["like"] = item.QuerySelector(".count.like > .value")?.InnerHtml?.Replace(",", "");
+                info["mylist"] = item.QuerySelector(".count.mylist > .value")?.InnerHtml?.Replace(",", "");
 
-                var thumbnailUrl = item.Groups["thumbnail"].Value;
-                if (thumbnailUrl[thumbnailUrl.Length - 2] == '.')
+                var thumbnailUrl = item.QuerySelector(@"img[class=""thumb""]")?.GetAttribute("src");
+                if (thumbnailUrl != null && thumbnailUrl[thumbnailUrl.Length - 2] == '.')
                     thumbnailUrl = thumbnailUrl.Substring(0, thumbnailUrl.Length - 2);
-
                 info["thumbnail"] = thumbnailUrl;
 
                 result.Add(info);
